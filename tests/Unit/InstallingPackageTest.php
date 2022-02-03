08b90bc77755026e9a0d3d5dd4efd66f787f07e2
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2022-02-03 12:41:10 +0100

    🔥 Removing unused import

--- tests/Unit/InstallingPackageTest.php
+++ tests/Unit/InstallingPackageTest.php
@@ -2,7 +2,6 @@
 namespace Henrotaym\VersioningPackageTemplate\Tests\Unit;
 
 use Henrotaym\VersioningPackageTemplate\Tests\TestCase;
-use Henrotaym\VersioningPackageTemplate\Facades\Package;
 use Henrotaym\LaravelPackageVersioning\Testing\Traits\InstallPackageTest;
 
 class InstallingPackageTest extends TestCase
